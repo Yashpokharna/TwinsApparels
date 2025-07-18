af37f33e0fac2f687195268b9203c4bfee31aef6
Yash Pokharna <yashpokharna2002@gmail.com>
2025-07-18 14:50:45 +0530

    Changes

--- src/components/QualityFeatures.js
+++ src/components/QualityFeatures.js
@@ -2,10 +2,10 @@ import Image from "next/image";
 
 const QualityFeature = ({ imgSrc, title, desc }) => {
   return (
-    <div className="flex items-start lg:flex-row sm:flex-col gap-1 sm:gap-4 lg:gap-8">
+    <div className="flex items-start gap-1 lg:flex-row sm:flex-col sm:gap-4 lg:gap-8">
       <Image src={imgSrc} width={80} height={80} alt="features" />
       <div className="leading-loose">
-        <h3 className="text-lg md:text-xl font-semibold mb-4">{title}</h3>
+        <h3 className="mb-4 text-lg font-semibold md:text-xl">{title}</h3>
         <p>{desc}</p>
       </div>
     </div>
@@ -14,30 +14,30 @@ const QualityFeature = ({ imgSrc, title, desc }) => {
 
 const QualityFeatures = () => {
   return (
-    <section className="container mx-auto px-5 md:px-16">
-      <span className="service-name text-center ">QUALITY FEATURES</span>
-      <h2 className="title text-center ">Amazing useful features</h2>
+    <section className="container px-5 mx-auto md:px-16">
+      <span className="text-center service-name ">OUR PRODUCTS</span>
+      <h2 className="text-center title ">Style. Comfort. Quality. Always.</h2>
 
       <div className="grid gap-x-10 gap-y-16 sm:grid-cols-2 mt-10 md:mt-20 lg:w-[88%] mx-auto">
         <QualityFeature
-          imgSrc="/features/1.svg"
-          title="Fast Performance"
-          desc="Let's just get this out of the way - there will always be a kit version of Edu flow. Paid subscriptions allow us to continue helping learners around the world."
-        />
-        <QualityFeature
           imgSrc="/features/2.svg"
-          title="Partnership deal"
-          desc="Let's just get this out of the way - there will always be a kit version of Edu flow. Paid subscriptions allow us to continue helping learners around the world."
+          title="Formal Wear"
+          desc="• Men’s Formal Trouser • Women Formal Trouser • Men’s Formal Shirts • Men & Women Blazer • Uniform Blazer."
         />
         <QualityFeature
           imgSrc="/features/3.svg"
-          title="Pro Subscription"
-          desc="Let's just get this out of the way - there will always be a kit version of Edu flow. Paid subscriptions allow us to continue helping learners around the world."
+          title="Casual Wear"
+          desc="• Men’s Cotton Trouser • Chinos • Men’s Five Pocket • Men’s Flexi Waist Trouser • Draw Cord Trouser • Men’s Casual Shirts • Shorts for Men & Women"
+        />
+        <QualityFeature
+          imgSrc="/features/1.svg"
+          title="Uniform & Workwear"
+          desc="• Uniform/Cargo Pants/Work Wear • Uniform Blazer"
         />
         <QualityFeature
           imgSrc="/features/4.svg"
-          title="Customer Support"
-          desc="Let's just get this out of the way - there will always be a kit version of Edu flow. Paid subscriptions allow us to continue helping learners around the world."
+          title="Stretch & Comfort Wear"
+          desc="• Men’s Flexi Waist Trouser • Draw Cord Trouser • Shorts for Men & Women"
         />
       </div>
     </section>
--- src/components/Work.js
+++ src/components/Work.js
@@ -3,14 +3,14 @@ import Image from "next/image";
 const WorkCard = ({ num, title }) => {
   return (
     <div className="w-[90%] sm:w-4/5 mx-auto md:mx-0 md:w-full flex flex-col md:gap-5 gap-3 text-center md:text-left">
-      <span className="md:mx-0 mx-auto text-3xl w-fit font-bold text-blue-800 bg-white rounded-full py-4 px-4">
+      <span className="px-4 py-4 mx-auto text-3xl font-bold text-blue-800 bg-white rounded-full md:mx-0 w-fit">
         {num}
       </span>
       <h2 className="text-xl font-semibold leading-relaxed">{title}</h2>
-      <p className="leading-loose">
+      {/* <p className="leading-loose">
         Get your blood tests delivered at home collect a sample from the your
         blood tests.
-      </p>
+      </p> */}
     </div>
   );
 };
@@ -18,19 +18,19 @@ const WorkCard = ({ num, title }) => {
 const Work = () => {
   return (
     <section className="w-full bg-rose-500 text-white bg-[url('/work/workbg.png')] bg-cover bg-no-repeat bg-center">
-      <div className="flex flex-col gap-10 lg:gap-16 container mx-auto md:px-16 px-5 py-12 sm:py-20 md:py-36">
+      <div className="container flex flex-col gap-10 px-5 py-12 text-center lg:gap-16 md:px-16 sm:py-20 md:py-36">
         <div>
-          <span className="uppercase block font-semibold text-sm tracking-widest text-center text-rose-200">
-            WHATS THE FUNCTION
+          <span className="block text-lg font-semibold tracking-widest text-center uppercase">
+            OPERATION&apos;S OVERVIEW
           </span>
-          <h2 className="text-2xl sm:text-4xl font-semibold my-3 text-center ">
+          <h2 className="my-3 text-2xl font-semibold text-center sm:text-4xl ">
             Let&apos;s see how it works
           </h2>
         </div>
 
-        <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-10 md:gap-5">
-          <div className="relative">
-            <WorkCard num="01" title="Set disbursement Instructions" />
+        <div className="grid gap-10 md:grid-cols-2 lg:grid-cols-4 md:gap-5">
+          <div className="relative mx-14">
+            <WorkCard num="01" title="Yarn Sourcing" />
             <Image
               src={"/work/arrow.svg"}
               width={210}
@@ -40,10 +40,9 @@ const Work = () => {
             />
           </div>
 
-          <div className="relative">
+          <div className="relative mx-14">
             <WorkCard
-              num="02"
-              title="Assembly retrieves funds from your account"
+              num="02" title="Twisting"
             />
             <Image
               src={"/work/arrow.svg"}
@@ -53,8 +52,8 @@ const Work = () => {
               className="hidden absolute top-7 left-[4.8rem] xl:block rotate"
             />
           </div>
-          <div className="relative">
-            <WorkCard num="03" title="Assembly initiates disbursement" />
+          <div className="relative mx-14">
+            <WorkCard num="03" title="Dyeing" />
             <Image
               src={"/work/arrow.svg"}
               width={205}
@@ -63,7 +62,85 @@ const Work = () => {
               className="hidden absolute top-2 left-[4.7rem] xl:block"
             />
           </div>
-          <WorkCard num="04" title="Customer receives funds payment" />
+          <div className="mx-14">
+          <WorkCard num="04" title="Winding"/>
+          </div>
+        </div>
+        <div className="grid gap-10 md:grid-cols-2 lg:grid-cols-4 md:gap-5">
+          <div className="relative mx-14">
+            <WorkCard num="05" title="Weaving" />
+            <Image
+              src={"/work/arrow.svg"}
+              width={210}
+              height={300}
+              alt="arrow"
+              className="hidden absolute top-2 left-[4.5rem] xl:block"
+            />
+          </div>
+
+          <div className="relative mx-14">
+            <WorkCard
+              num="06" title="Fabric Checking"
+            />
+            <Image
+              src={"/work/arrow.svg"}
+              width={205}
+              height={300}
+              alt="arrow"
+              className="hidden absolute top-7 left-[4.8rem] xl:block rotate"
+            />
+          </div>
+          <div className="relative mx-14">
+            <WorkCard num="07" title="Stitching" />
+            <Image
+              src={"/work/arrow.svg"}
+              width={205}
+              height={300}
+              alt="arrow"
+              className="hidden absolute top-2 left-[4.7rem] xl:block"
+            />
+          </div>
+          <div className="mx-14">
+          <WorkCard num="08" title="Inspection" />
+          </div>
+        </div>
+        <div className="grid gap-10 md:grid-cols-2 lg:grid-cols-4 md:gap-5">
+          <div className="relative mx-14">
+            <WorkCard num="09" title="Trimming & Checking" />
+            <Image
+              src={"/work/arrow.svg"}
+              width={210}
+              height={300}
+              alt="arrow"
+              className="hidden absolute top-2 left-[4.5rem] xl:block"
+            />
+          </div>
+
+          <div className="relative mx-14">
+            <WorkCard
+              num="10" title="Packing"
+            />
+            <Image
+              src={"/work/arrow.svg"}
+              width={205}
+              height={300}
+              alt="arrow"
+              className="hidden absolute top-7 left-[4.8rem] xl:block rotate"
+            />
+          </div>
+          <div className="relative mx-14">
+            <WorkCard num="11" title="Inspection" />
+            <Image
+              src={"/work/arrow.svg"}
+              width={205}
+              height={300}
+              alt="arrow"
+              className="hidden absolute top-2 left-[4.7rem] xl:block"
+            />
+          </div>
+          <div className="mx-14">
+          <WorkCard num="12" title="Final Shipment" />
+          </div>
         </div>
       </div>
     </section>
