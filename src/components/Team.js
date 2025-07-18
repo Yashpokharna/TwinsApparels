350ab7b72fc9ffacd842f36554e2324199b74e5b
Yash Pokharna <yashpokharna2002@gmail.com>
2025-07-18 21:22:12 +0530

    Team page update

--- src/components/Team.js
+++ src/components/Team.js
@@ -1,10 +1,10 @@
 import Image from "next/image";
 import FacebookRoundedIcon from "@mui/icons-material/FacebookRounded";
-import GitHubIcon from "@mui/icons-material/GitHub";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import Link from "next/link";
+import { Instagram } from "@mui/icons-material";
 
-const TeamCard = ({ imgSrc, name, title }) => {
+const TeamCard = ({ imgSrc, name, title, facebook, instagram, linkedin }) => {
   return (
     <div className="relative flex flex-col gap-1 md:hover:shadow-lg rounded-xl md:py-10 team-card md:cursor-pointer">
       <Image
@@ -16,22 +16,23 @@ const TeamCard = ({ imgSrc, name, title }) => {
       />
       <h2 className="text-base font-semibold text-center sm:text-xl">{name}</h2>
       <p className="text-sm text-center sm:text-base">{title}</p>
+
       <div className="flex gap-3 mx-auto md:flex-col md:absolute md:bottom-12 md:right-8 md:translate-y-10 icons md:transition-all md:duration-500 md:opacity-0 md:mx-0 md:text-rose-600">
-        <Link
-          target="_blank"
-          href="https://www.facebook.com/profile.php?id=100017192357822&sk"
-        >
-          <FacebookRoundedIcon className="text-xl cursor-pointer hover:text-rose-600 md:hover:text-gray-500" />
-        </Link>
-        <Link target="_blank" href="https://github.com/NaseemKhan005">
-          <GitHubIcon className="text-xl cursor-pointer hover:text-rose-600 md:hover:text-gray-500" />
-        </Link>
-        <Link
-          target="_blank"
-          href="https://www.linkedin.com/in/naseem-khan-275275258/"
-        >
-          <LinkedInIcon className="text-xl cursor-pointer hover:text-rose-600 md:hover:text-gray-500" />
-        </Link>
+        {facebook && (
+          <Link href={facebook} target="_blank">
+            <FacebookRoundedIcon className="text-xl cursor-pointer hover:text-rose-600 md:hover:text-gray-500" />
+          </Link>
+        )}
+        {instagram && (
+          <Link href={instagram} target="_blank">
+            <Instagram className="text-xl cursor-pointer hover:text-rose-600 md:hover:text-gray-500" />
+          </Link>
+        )}
+        {linkedin && (
+          <Link href={linkedin} target="_blank">
+            <LinkedInIcon className="text-xl cursor-pointer hover:text-rose-600 md:hover:text-gray-500" />
+          </Link>
+        )}
       </div>
     </div>
   );
@@ -40,38 +41,26 @@ const TeamCard = ({ imgSrc, name, title }) => {
 const Team = () => {
   return (
     <section className="container px-5 mx-auto md:px-16 lg:px-24" id="Team">
-      <span className="text-center service-name ">OUR TEAM</span>
-      <h2 className="mx-auto text-center title md:w-1/2">
-        Driven By Teamwork
-      </h2>
+      <span className="text-center service-name">OUR TEAM</span>
+      <h2 className="mx-auto text-center title md:w-1/2">Driven By Teamwork</h2>
 
       <div className="grid grid-cols-2 mx-auto mt-16 lg:grid-cols-2 gap-y-8 sm:gap-8">
         <TeamCard
-          imgSrc={"/team/1.png"}
+          imgSrc="/team/1.png"
           name="Satyam Goyal"
-          title="CEO and Founder"
-        />
-        <TeamCard imgSrc={"/team/2.png"} name="Shivam Goyal" title="Founder" />
-        {/* <TeamCard
-          imgSrc={"/team/3.png"}
-          name="Aaron Nunez"
-          title="Web Designer"
-        /> */}
-        {/* <TeamCard
-          imgSrc={"/team/4.png"}
-          name="Lina Jutila"
-          title="Web Developer"
+          title="Founder"
+          facebook="https://www.facebook.com/satyam.goyal.393"
+          instagram="https://www.instagram.com/satyam__goyal/"
+          linkedin="https://www.linkedin.com/in/satyam-goyal-4082792ba/"
         />
         <TeamCard
-          imgSrc={"/team/5.png"}
-          name="Saimon Harmer"
-          title="CEO and Founder"
+          imgSrc="/team/2.png"
+          name="Shivam Goyal"
+          title="Founder"
+          facebook="https://www.facebook.com/shivam.09.goyal" // change to actual
+          instagram="https://www.instagram.com/shiivamgoyall/" // change to actual
+          // linkedin="https://www.linkedin.com/in/shivam-goyal" // change to actual
         />
-        <TeamCard
-          imgSrc={"/team/6.png"}
-          name="Aaron Nunez"
-          title="Web Designer"
-        /> */}
       </div>
     </section>
   );
