5383e658ee778ef0fe397959c22aa9bcdaf1e9ee
Yashpokharna <yashpokharna2002@gmail.com>
2026-01-26 17:18:30 +0530

    Favicon update

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,7 @@
 "use client";
 import Community from "@/components/Community";
-import CoreFeatures from "@/components/About";
-import Features from "@/components/Features";
 import HeroSection from "@/components/HeroSection";
-import Pricing from "@/components/Pricing";
-import QualityFeatures from "@/components/Products";
-import Services from "@/components/Machinery";
-import Subscribe from "@/components/Subscribe";
 import Team from "@/components/Team";
-import Testimonials from "@/components/Testimonials";
 import Work from "@/components/Work";
 import About from "@/components/About";
 import Machinery from "@/components/Machinery";
